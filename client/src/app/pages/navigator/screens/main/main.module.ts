f98cb4ddb41f66ec1b8602cee0a302cdfad7e930
Duy Lu Hoang <duy.lh@itss.edu.vn>
2023-08-17 12:05:14 +0700

    add home

--- client/src/app/pages/navigator/screens/main/main.module.ts
+++ client/src/app/pages/navigator/screens/main/main.module.ts
@@ -3,27 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
-<<<<<<< HEAD:client/src/app/pages/main/main.module.ts
-import { HomeComponent } from './components/home/home.component';
-import { MaterialModule } from 'src/shared/material.module';
-
-
-@NgModule({
-  declarations: [
-    MainComponent,
-    HomeComponent
-  ],
-  imports: [
-    CommonModule,
-    MainRoutingModule,
-    MaterialModule
-  ]
-=======
 import { SharedModule } from 'src/shared/shared.module';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  declarations: [MainComponent],
+  declarations: [MainComponent, HomeComponent],
   imports: [CommonModule, MainRoutingModule, SharedModule],
->>>>>>> d17543ce21ecedb465d2f2ac37e119446b8a4918:client/src/app/pages/navigator/screens/main/main.module.ts
 })
 export class MainModule {}
