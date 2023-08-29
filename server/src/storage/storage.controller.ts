46c3350f801bea0a3997ebe2835216c98cb8d36b
Duy Lu Hoang <duy.lh@itss.edu.vn>
2023-08-29 10:53:30 +0700

    api storage & apply middleware

--- server/src/storage/storage.controller.ts
+++ server/src/storage/storage.controller.ts
@@ -1,57 +1,61 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFiles, ParseFilePipe, MaxFileSizeValidator, FileTypeValidator } from '@nestjs/common';
+import {
+  Controller,
+  FileTypeValidator,
+  Get,
+  MaxFileSizeValidator,
+  Param,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
 import { StorageService } from './storage.service';
-import { CreateStorageDto } from './dto/create-storage.dto';
-import { UpdateStorageDto } from './dto/update-storage.dto';
-import { FilesInterceptor } from '@nestjs/platform-express';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import { Storage } from './entities/storage.entity';
 
 @Controller('storage')
 export class StorageController {
   constructor(private readonly storageService: StorageService) {}
 
-  @Post()
-  create(@Body() createStorageDto: CreateStorageDto) {
-    return this.storageService.create(createStorageDto);
+  @Post('upload/:folderName')
+  @UseInterceptors(FilesInterceptor('files'))
+  async uploadFiles(
+    @UploadedFiles(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 1024 * 1024 * 5 }),
+          new FileTypeValidator({
+            fileType: '.(jpg|jpeg|png)',
+          }),
+        ],
+      }),
+    )
+    files: Express.Multer.File[],
+    @Param('folderName') folderName: string,
+  ): Promise<{ urls: string[] }> {
+    try {
+      const urls = await this.storageService.uploadFiles(files, folderName);
+      await this.storageService.saveFileUrlToDatabase({
+        folderName,
+        urls,
+      });
+      return { urls };
+    } catch (error) {
+      throw error;
+    }
   }
 
-  // @Post('upload/:storage')
-  // @UserInterceptors(FilesInterceptor('files'))
-  // async uploadFiles(
-  //   @UploadedFiles(
-  //     new ParseFilePipe({
-  //       validators:[
-  //         new MaxFileSizeValidator({ maxSize: 1024*1024*5}),
-  //         new FileTypeValidator({ fileType: '.(jpg|jpeg|png)',}),
-  //       ],
-  //     }),
-  //   )
-  //   files: Express.Multer.File[],
-  //   @Param('folderName') folderName: string,): Promise<{urls: string}>{
-  //     try {
-  //       const urls = await this.storageService.uploadFiles(files, folderName);
-  //       await this.storageService.saveFileUrlToDatabase({folderName,urls});
-  //       return {urls};
-  //     }catch(error){
-  //       throw error
-  //     }
-  //   }
-
-  @Get()
-  findAll() {
-    return this.storageService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.storageService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateStorageDto: UpdateStorageDto) {
-    return this.storageService.update(+id, updateStorageDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.storageService.remove(+id);
+  @Get(':folderName')
+  async getFiles(@Param('folderName') folderName: string): Promise<Storage[]> {
+    try {
+      const files = await this.storageService.getFilesByFolderName(folderName);
+      if (!files) {
+        return [];
+      }
+      return files;
+    } catch (error) {
+      throw error;
+    }
   }
 }
--- server/src/storage/storage.service.ts
+++ server/src/storage/storage.service.ts
@@ -1,71 +1,80 @@
 import { HttpException, Injectable } from '@nestjs/common';
-import { CreateStorageDto } from './dto/create-storage.dto';
-import { UpdateStorageDto } from './dto/update-storage.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import * as admin from 'firebase-admin';
 import { v4 as uuidv4 } from 'uuid';
+
+import { CreateStorageDto } from './dto/create-storage.dto';
+import { Storage } from './entities/storage.entity';
+
+import * as admin from 'firebase-admin';
 
 @Injectable()
 export class StorageService {
-  // constructor(
-  //   @InjectModel(Storage.name) private storageModel: Model<Storage>
-  // ){}
+  constructor(
+    @InjectModel(Storage.name) private storageModel: Model<Storage>,
+  ) {}
 
-  //   async uploadFiles( files:  Express.Multer.File[], folderName: string,): Promise<string[]>{
-  //     try{
-  //       const bucketName = 'gs://soliqs-web23s.appspot.com/';
-  //       const publicUrls: string[] = [];
+  async uploadFiles(
+    files: Express.Multer.File[],
+    folderName: string,
+  ): Promise<string[]> {
+    try {
+      const bucketName = 'soliqs-web23s.appspot.com';
+      const publicUrls: string[] = [];
 
-  //       await Promise.all(
-  //         files.map(async (file)=>{
-  //           const fileName = `images/${folderName}/${uuidv4()}-${file.originalname}`;
-  //           const fileUpload = admin.storage().bucket(bucketName).file(fileName);
-  //           const blobStream = fileUpload.createWriteStream({
-  //             metadata:{
-  //               contentType: file.mimetype,
-  //             }, 
-  //           });
-  //           await new Promise((resolve,reject) => {
-  //             blobStream.on('error', (error)=>{
-  //               reject(error);
-  //             });
-  //             blobStream.on('finish', async () =>{
-  //               const [imageURL] = await fileUpload.getSignedUrl({
-  //                 action: 'read',
-  //                 expires:'13-01-2101',
-  //               });
-  //               publicUrls.push(imageURL);
-  //               resolve(imageURL);
-  //             });
-  //             blobStream.end(file.buffer);
-  //           });
-  //         }),
-  //       )
-  //       return publicUrls;                                                
-  //     }catch(error){
-  //       throw new HttpException(error.message, error.status);
-  //     }
-  //   }
-  //   async saveFileUrlToDatabase(){}
+      await Promise.all(
+        files.map(async (file) => {
+          const fileName = `images/${folderName}/${uuidv4()}-${
+            file.originalname
+          }`;
+          const fileUpload = admin.storage().bucket(bucketName).file(fileName);
 
-  create(createStorageDto: CreateStorageDto) {
-    return 'This action adds a new storage';
+          const blobStream = fileUpload.createWriteStream({
+            metadata: {
+              contentType: file.mimetype,
+            },
+          });
+
+          await new Promise((resolve, reject) => {
+            blobStream.on('error', (error) => {
+              reject(error);
+            });
+
+            blobStream.on('finish', async () => {
+              const [imageURL] = await fileUpload.getSignedUrl({
+                action: 'read',
+                expires: '12-01-2101',
+              });
+              publicUrls.push(imageURL);
+              resolve(imageURL);
+            });
+
+            blobStream.end(file.buffer);
+          });
+        }),
+      );
+      return publicUrls;
+    } catch (error) {
+      throw new HttpException(error.message, error.status);
+    }
   }
 
-  findAll() {
-    return `This action returns all storage`;
+  async saveFileUrlToDatabase(
+    createStorageDto: CreateStorageDto,
+  ): Promise<Storage> {
+    try {
+      const fileUrl = new this.storageModel(createStorageDto);
+      return await fileUrl.save();
+    } catch (error) {
+      throw new HttpException(error.message, error.status);
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} storage`;
-  }
-
-  update(id: number, updateStorageDto: UpdateStorageDto) {
-    return `This action updates a #${id} storage`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} storage`;
+  async getFilesByFolderName(folderName: string): Promise<Storage[]> {
+    try {
+      return await this.storageModel.find({ folderName }).exec();
+    } catch (error) {
+      throw error;
+    }
   }
 }
