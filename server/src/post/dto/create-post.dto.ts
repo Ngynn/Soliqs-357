41bb0e757010b6573dff57e22f7ef6f1014a1059
Duy Lu Hoang <duy.lh@itss.edu.vn>
2023-08-30 12:46:48 +0700

    post api

--- server/src/post/dto/create-post.dto.ts
+++ server/src/post/dto/create-post.dto.ts
@@ -1,18 +1,14 @@
-/* eslint-disable prettier/prettier */
 export class CreatePostDto {
-    constructor(
-        public id: string,
-        public authorId: string,
-        public authorName: string,
-        public authorUsername: string,
-        public authorAvatar: string,
-        public content: string,
-        public likes: string[],
-        public comments: string[],
-        public shares: string[],
-        public media: string[],
-        public tags: string[],
-        public bookmarks: string[],
-        public isPrivate: boolean,
-    ) { }
+  constructor(
+    public id: string,
+    public authorId: string,
+    public content: string,
+    public media: string[],
+    public tags: string[],
+    public likes: string[],
+    public comments: string[],
+    public shares: string[],
+    public bookmarks: string[],
+    public isPrivate: boolean,
+  ) {}
 }
