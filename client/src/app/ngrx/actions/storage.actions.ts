fe82a10b5f3d379f56bb8382bbec076fe42ae36a
minhkhoa1311 <khoa.nm00716@sinhvien.hoasen.edu.vn>
2023-08-30 13:06:51 +0700

    trước khi pull api mới

---
+++ client/src/app/ngrx/actions/storage.actions.ts
@@ -0,0 +1,4 @@
+import { createAction } from "@ngrx/store";
+
+export const create = createAction('[storage] Create Success');
+export const createSuccess = createAction('[storage] Create Success');
